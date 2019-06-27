3055fded093cfd77b73c5944242397a2e5e32bd7
Hammodi <mmodimike@gmail.com>
2019-06-27 18:26:49 +0300

    add dir
    add dir

---
+++ src/main/java/com/hammodi/hammodiz/HammodizApplication.java
@@ -0,0 +1,13 @@
+package com.hammodi.hammodiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HammodizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HammodizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hammodi/hammodiz/HammodizApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hammodi.hammodiz;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HammodizApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
